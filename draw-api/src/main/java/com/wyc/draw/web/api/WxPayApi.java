0e21f6a8cafd52893026f552620d3eaba08f534b
System Administrator <root@dawang.local>
2017-02-20 14:40:28 +0800

    ss

--- draw-api/src/main/java/com/wyc/draw/web/api/WxPayApi.java
+++ draw-api/src/main/java/com/wyc/draw/web/api/WxPayApi.java
@@ -69,7 +69,7 @@ public class WxPayApi {
 		PaySuccess paySuccess2 = paySuccessService.findOneByOutTradeNo(paySuccess.getOutTradeNo());
 		if(paySuccess2==null){
 			if(paySuccess.getNonceStr().equals("1add1a30ac87aa2db72f57a2375d8f22")&&paySuccess.getResultCode().equals("SUCCESS")){
-				RedPacket redPacket = redPacketService.findOneByOutTradeNo(paySuccess2.getOutTradeNo());
+				RedPacket redPacket = redPacketService.findOneByOutTradeNo(paySuccess.getOutTradeNo());
 				redPacket.setIsPay(1);
 				redPacket.setIsDisplay(1);
 				redPacketService.update(redPacket);
