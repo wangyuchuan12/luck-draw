cda6c97d97fc4a1554cc7eec0366a5cc8020e5d7
System Administrator <root@dawang.local>
2017-08-03 12:26:50 +0800

    添加appid

--- draw-webmain/src/main/webapp/js/myscript1.0.js
+++ draw-webmain/src/main/webapp/js/myscript1.0.js
@@ -462,9 +462,26 @@ function wxOnMenuShareAppMessage(title,desc,link,imgUrl,type,dataUrl,callback){
 }
 
 
+function onBridgeReady(appId,timestamp,nonceStr,pack,signType,paySign){
+	   WeixinJSBridge.invoke(
+	       'getBrandWCPayRequest', {
+	           "appId":"wx2421b1c4370ec43b",     //公众号名称，由商户传入     
+	           "timeStamp":"1395712654",         //时间戳，自1970年以来的秒数     
+	           "nonceStr":"e61463f8efa94090b1f366cccfbbb444", //随机串     
+	           "package":"prepay_id=u802345jgfjsdfgsdg888",     
+	           "signType":"MD5",         //微信签名方式：     
+	           "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+	       },
+	       function(res){     
+	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+	       }
+	   ); 
+}
+
 function wxPay(timestamp,nonceStr,pack,signType,paySign,callback){
 	wx.ready(function(){
 		wx.chooseWXPay({
+			appId:"wxb07bdf79ef8f00a9",
 			timestamp:timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
 		    nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
 		    package: pack, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
