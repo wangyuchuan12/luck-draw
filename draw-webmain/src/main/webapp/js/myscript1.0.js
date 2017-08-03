4efdd9737585e36fb28fbcc35730d779cf0d9f42
System Administrator <root@dawang.local>
2017-08-03 12:45:14 +0800

    success

--- draw-webmain/src/main/webapp/js/myscript1.0.js
+++ draw-webmain/src/main/webapp/js/myscript1.0.js
@@ -484,7 +484,8 @@ function onBridgeReady(timestamp,nonceStr,pack,signType,paySign,callback){
 			    	callback.failure();
 			    }
 	       },
-	       function(res){     
+	       function(res){
+	    	   alert("res:"+res);
 	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
 	       }
 	   ); 
