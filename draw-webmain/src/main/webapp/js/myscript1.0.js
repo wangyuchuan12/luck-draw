ade34c5c3cc412603fc938b7f40ae74eeb67d397
System Administrator <root@dawang.local>
2017-08-03 12:39:10 +0800

    paysuccess

--- draw-webmain/src/main/webapp/js/myscript1.0.js
+++ draw-webmain/src/main/webapp/js/myscript1.0.js
@@ -462,7 +462,7 @@ function wxOnMenuShareAppMessage(title,desc,link,imgUrl,type,dataUrl,callback){
 }
 
 
-function onBridgeReady(timestamp,nonceStr,pack,signType,paySign){
+function onBridgeReady(timestamp,nonceStr,pack,signType,paySign,callback){
 	   WeixinJSBridge.invoke(
 	       'getBrandWCPayRequest', {
 	           "appId":"wxb07bdf79ef8f00a9",     //公众号名称，由商户传入     
@@ -470,7 +470,19 @@ function onBridgeReady(timestamp,nonceStr,pack,signType,paySign){
 	           "nonceStr":nonceStr, //随机串     
 	           "package":pack,     
 	           "signType":signType,         //微信签名方式：     
-	           "paySign":paySign //微信签名 
+	           "paySign":paySign, //微信签名 
+	           success: function (res) {
+			    	callback.success();
+			    	
+			    },
+			    
+			    cancel:function(res){
+			    	callback.cancel();
+			    },
+			    
+			    fail:function(res){
+			    	callback.failure();
+			    }
 	       },
 	       function(res){     
 	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
