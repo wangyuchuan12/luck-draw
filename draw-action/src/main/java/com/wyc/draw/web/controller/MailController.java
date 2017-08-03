e418dadd5a29f039c8303adbf9e11557102ae540
System Administrator <root@dawang.local>
2017-08-03 18:53:14 +0800

    mall

--- draw-action/src/main/java/com/wyc/draw/web/controller/MailController.java
+++ draw-action/src/main/java/com/wyc/draw/web/controller/MailController.java
@@ -5,10 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.wyc.annotation.HandlerAnnotation;
+import com.wyc.draw.filter.DrawUserFilter;
+
 @Controller
 @RequestMapping(value="/view/draw/mall/")
 public class MailController {
 	
+	
+	@HandlerAnnotation(hanlerFilter=DrawUserFilter.class)
 	@RequestMapping(value="mainView")
 	public String mainView(HttpServletRequest httpServletRequest){
 		return "mall";
